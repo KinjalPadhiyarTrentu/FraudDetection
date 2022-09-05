adb070e1958f938ff0027430135a171d63f4ffb8
KinjalPadhiyarTrentu <99310137+KinjalPadhiyarTrentu@users.noreply.github.com>
2022-09-04 23:47:48 -0400

    Add files via upload

--- FaceDetectionUsingComputerVision/Designing/src/com/VO/classVO.java
+++ FaceDetectionUsingComputerVision/Designing/src/com/VO/classVO.java
@@ -1,1 +1,49 @@
+package com.VO;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="classmaster")
+public class classVO
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="classId")
+	private int classId;
+	
+	@Column(name="className")
+	private String className;
+	
+	public int getclassId() {
+		return classId;
+	}
+
+	public void setclassId(int classId) {
+		this.classId = classId;
+	}
+
+	public String getclassName() {
+		return className;
+	}
+
+	public void setclassName(String className) {
+		this.className = className;
+	}
+
+	/*public String getclassDepartment() {
+		return classDepartment;
+	}
+
+	public void setclassDepartment(String classDepartment) {
+		this.classDepartment = classDepartment;
+	}
+
+	@Column(name="classDepartment")
+	private String classDepartment;
+*/
+	}
---
+++ FaceDetectionUsingComputerVision/Designing/src/com/VO/registerVO.java
@@ -0,0 +1,129 @@
+package com.VO;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="registration")
+public class registerVO 
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="StudentId")
+	private int studentId;
+	
+	@Column(name="Name")
+	private String studentName;
+	
+	@Column(name="UserName")
+	private String studentUsername;
+	
+	@Column(name="Email")
+	private String studentEmail;
+	
+	@Column(name="studentPassword")
+	private String studentPassword;
+	
+	@Column(name="Phone")
+	private String studentPhone;
+	
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentUsername() {
+		return studentUsername;
+	}
+
+	public void setStudentUsername(String studentUsername) {
+		this.studentUsername = studentUsername;
+	}
+
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+
+	public String getStudentPassword() {
+		return studentPassword;
+	}
+
+	public void setStudentPassword(String studentPassword) {
+		this.studentPassword = studentPassword;
+	}
+
+	public String getStudentPhone() {
+		return studentPhone;
+	}
+
+	public void setStudentPhone(String studentPhone) {
+		this.studentPhone = studentPhone;
+	}
+
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+
+	public String getStudentCity() {
+		return studentCity;
+	}
+
+	public void setStudentCity(String studentCity) {
+		this.studentCity = studentCity;
+	}
+
+	public String getStudentState() {
+		return studentState;
+	}
+
+	public void setStudentState(String studentState) {
+		this.studentState = studentState;
+	}
+
+	public String getStudentCountry() {
+		return studentCountry;
+	}
+
+	public void setStudentCountry(String studentCountry) {
+		this.studentCountry = studentCountry;
+	}
+
+	@Column(name="Address")
+	private String studentAddress;
+	
+	@Column(name="City")
+	private String studentCity;
+	
+	@Column(name="State")
+	private String studentState;
+	
+	@Column(name="Country")
+	private String studentCountry;
+	
+
+	
+	
+}	
